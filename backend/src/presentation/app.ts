ed699d802f4dd8fa70e3727644c6cab96263fb11
Lucas Santos <lhs.santoss@gmail.com>
2019-02-15 20:57:47 -0200

    Add shipId on dock/undock and reorder instances

--- backend/src/presentation/app.ts
+++ backend/src/presentation/app.ts
@@ -10,19 +10,19 @@ import { PortService } from '../services/PortService'
 export const app = expresso(async (app: Express, config: any) => {
   const connection = await mongodb.createConnection(config.database.mongodb)
 
-  const shipRepository = new ShipRepository(connection)
-  const shipService = new ShipService(shipRepository)
-
   const portRepository = new PortRepository(connection)
   const portService = new PortService(portRepository)
+
+  const shipRepository = new ShipRepository(connection)
+  const shipService = new ShipService(shipRepository, portService)
 
   app.post('/ships', routes.ship.create.factory(shipService, portService))
   app.delete('/ships/:shipId', routes.ship.delete.factory(shipService, portService))
   app.get('/ships/:shipId', routes.ship.find.factory(shipService))
   app.get('/ships', routes.ship.getAll.factory(shipService))
   app.get('/ships/:shipId/events', routes.ship.getEvents.factory(shipService))
-  app.put('/ships/dock/:portId', routes.ship.dock.factory(shipService))
-  app.put('/ships/depart', routes.ship.depart.factory(shipService))
+  app.put('/ships/:shipId/dock/:portId', routes.ship.dock.factory(shipService))
+  app.put('/ships/:shipId/depart', routes.ship.depart.factory(shipService))
 
   app.post('/ports', routes.port.create.factory(portService))
   app.delete('/ports/:portId', routes.port.delete.factory(portService, shipService))
