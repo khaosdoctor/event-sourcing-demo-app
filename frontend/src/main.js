50e7bbed6339249a3ecb9e3122e42a0e226ccfad
Lucas Santos <lhs.santoss@gmail.com>
2019-02-18 17:42:56 -0300

    Add filter

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
 import services from './plugins/services'
+import VueFilterDateFormat from 'vue-filter-date-format'
 
 Vue.config.productionTip = false
 
+Vue.use(VueFilterDateFormat)
 Vue.use(services)
 
 new Vue({
