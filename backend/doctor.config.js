236c8640a37e9568e8c93df062db649eb5e8872a
Lucas Santos <lhs.santoss@gmail.com>
2019-02-22 12:43:36 -0300

    Update doctor config

--- backend/doctor.config.js
+++ backend/doctor.config.js
@@ -1,7 +1,7 @@
-const Ship = require('./dist/domain/ship/entity')
-const ShipRepository = require('./dist/data/repositories/ShipRepository')
-const Port = require('./dist/domain/port/entity')
-const PortRepository = require('./dist/data/repositories/PortRepository')
+const { Ship } = require('./dist/domain/ship/entity')
+const { ShipRepository } = require('./dist/data/repositories/ShipRepository')
+const { Port } = require('./dist/domain/port/entity')
+const { PortRepository } = require('./dist/data/repositories/PortRepository')
 
 module.exports = {
   mongodb: {
